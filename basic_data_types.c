4a94c772e2a5e35fc26c43c07011b30853cdb029
snehadeepaktandra <snehadeepaktandra@gmail.com>
2020-05-29 17:01:27 +0530

    added basic pointer operations

--- basic_data_types.c
+++ basic_data_types.c
@@ -20,6 +20,13 @@ int main()
 	float fl = -9999.999;
         double db = -1000000.1000000; 
 	long double ldb = -1000000.1000000;         
+
+	/*Pointers*/ 
+	int *p_a = &a; 
+	char *p_ch = &ch; 
+	float *p_fl = &fl; 
+	double *p_db = &db; 
+	
        
 	/*Printing variables*/ 
        printf("int a is : %d \n", a); 
@@ -53,6 +60,39 @@ int main()
 	printf("sizeof 'float' type is: %ld\n", sizeof(float)); 
 	printf("sizeof 'double' type is : %ld \n", sizeof(double));
 	printf("sizeof 'long double' is : %ld \n", sizeof(long double));
+
+	printf("\n"); 
+
+	/*dereferring Pointers*/ 
+	printf("dereferring integer pointer p_a : %d \n", *p_a); 
+	printf("dereferring charecter pointer p_ch : %c \n", *p_ch); 
+	printf("dereferring float pointer p_fl : %6.3f \n", *p_fl); 
+	printf("dereferring double pointer p_db : %7.7f \n", *p_db);
+
+	printf("\n"); 
+
+	/*printing size of pointers*/ 
+	printf("size of interger pointer : %ld \n", sizeof(int *)); 
+	printf("size of charecter pointer : %ld \n", sizeof(char *)); 
+	printf("size of float pointer : %ld \n", sizeof(float *)); 
+	printf("size of double pointer : %ld \n", sizeof(double *)); 
+	
+	printf("\n");
+
+	/*printing pointers*/ 
+	printf("Pointer p_a is : %p \n ", p_a); 
+	printf("Pointer p_ch is : %p \n ", p_ch); 
+	printf("Pointer p_fl is : %p \n ", p_fl); 
+	printf("Pointer p_db is : %p \n ", p_db);
+
+	printf("\n");
+
+	/*Incrementing Pointers*/ 
+	printf("Incrementing integer pointer : %p \n", ++p_a); 
+	printf("Incrementing charecter pointer : %p \n", ++p_ch); 
+	printf("Incrementing float pointer : %p \n", ++p_fl); 
+	printf("Incrementing double pointer : %p \n", ++p_db); 
+		
 
 return 0; 
 } 
@@ -103,6 +143,26 @@ sizeof 'unsigned char' type is: 1
 sizeof 'float' type is: 4
 sizeof 'double' type is : 8 
 sizeof 'long double' is : 16 
+
+dereferring integer pointer p_a : -2147483648 
+dereferring charecter pointer p_ch : c 
+dereferring float pointer p_fl : -9999.999 
+dereferring double pointer p_db : -1000000.1000000 
+
+size of interger pointer : 8 
+size of charecter pointer : 8 
+size of float pointer : 8 
+size of double pointer : 8 
+
+Pointer p_a is : 0x7ffd82b38e9c 
+ Pointer p_ch is : 0x7ffd82b38e97 
+ Pointer p_fl is : 0x7ffd82b38ea0 
+ Pointer p_db is : 0x7ffd82b38ea8 
+ 
+Incrementing integer pointer : 0x7ffd82b38ea0 
+Incrementing charecter pointer : 0x7ffd82b38e98 
+Incrementing float pointer : 0x7ffd82b38ea4 
+Incrementing double pointer : 0x7ffd82b38eb0
 */ 
 
 
